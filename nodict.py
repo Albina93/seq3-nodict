aa808e464168339b19166e835c880a3d3d576a21
Piero <madarp@pobox.com>
2020-06-26 14:26:29 -0400

    Add encrypted soln and code scaffold

--- nodict.py
+++ nodict.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+
+
+class Node:
+    """A class that represents a key/value pair, with a hash"""
+    def __init__(self, key, value=None):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+
+class NoDict:
+    def __init__(self, num_buckets=10):
+        pass
+
+    def __repr__(self):
+        """How this class renders its data when printed"""
+        pass
+
+    def add(self, key, value):
+        """Inserts a new key-value Node into the NoDict"""
+        pass
+
+    def get(self, key):
+        """Lookup the value of a key"""
+        pass
+
+    def __getitem__(self, key):
+        """Implements [] lookup operator"""
+        pass
+
+    def __setitem__(self, key, value):
+        """Implements [] assignment operator"""
+        pass
--- tests/test_nodict.py
+++ tests/test_nodict.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+"""
+Unit test cases for nodict.py
+Students should not need to modify this file.
+"""
+
+__author__ = "madarp"
+
+import sys
+import unittest
+import importlib
+import inspect
+
+# suppress __pycache__ and .pyc files
+sys.dont_write_bytecode = True
+
+# devs: change this to soln.nodict to test solution
+PKG_NAME = 'nodict'
+
+
+class TestNoDict(unittest.TestCase):
+    """Main test fixture for nodict module"""
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
